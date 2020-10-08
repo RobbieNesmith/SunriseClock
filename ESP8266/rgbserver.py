76207605493a509eb24846fac7edf4c3229da3cb
Robbie Nesmith <robbie@newbo.co>
2020-10-08 11:30:31 -0500

    Add back lerp function

--- ESP8266/rgbserver.py
+++ ESP8266/rgbserver.py
@@ -14,6 +14,16 @@ WAITING_FOR_FADE = 0
 FADING = 1
 MANUAL_MODE = 2
 
+def lerp(cs1, cs2, pos):
+  if pos < 0:
+    pos = 0
+  elif pos > 1:
+    pos = 1
+  r = cs1.r * (1 - pos) + cs2.r * pos
+  g = cs1.g * (1 - pos) + cs2.g * pos
+  b = cs1.b * (1 - pos) + cs2.b * pos
+  return ColorStop(r, g, b)
+
 def render_color_stop(cs, i2c):
   cmd = bytearray([int(cs.r), int(cs.g), int(cs.b)])
   i2c.writeto(8, cmd)
