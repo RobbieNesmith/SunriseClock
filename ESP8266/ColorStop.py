cfac41c31ae12e9696246ef63bba7f8aa912b6ac
Robbie Nesmith <bobberto1995@gmail.com>
2023-02-07 06:58:44 -0600

    Remove unused def lerp

--- ESP8266/ColorStop.py
+++ ESP8266/ColorStop.py
@@ -3,13 +3,3 @@ class ColorStop:
     self.r = r
     self.g = g
     self.b = b
-
-def lerp(cs1, cs2, pos):
-  if pos < 0:
-    pos = 0
-  elif pos > 1:
-    pos = 1
-  r = cs1.r * (1 - pos) + cs2.r * pos
-  g = cs1.g * (1 - pos) + cs2.g * pos
-  b = cs1.b * (1 - pos) + cs2.b * pos
-  return ColorStop(r, g, b)
